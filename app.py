bc348af60ddb16431631e12226c665540e6c98c7
Rohit Jain <rohit.jain@medable.com>
2026-08-27 15:58:48 +0530

    commit...

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import os
 import gradio as gr
 
 from rag import retrieve_and_answer, build_index, CONFIDENCE_THRESHOLD
@@ -45,4 +46,5 @@ demo = gr.ChatInterface(
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
